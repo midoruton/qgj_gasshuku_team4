c05f857e2f2f944ac7db9d64f185a2c7e7b82a55
midorun <midoruton0906@gmail.com>
2018-09-06 10:48:19 +0900

    舌ができたのでコミット

---
+++ Assets/InputToungeAction.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputToungeAction : MonoBehaviour {
+
+    [SerializeField] private float toungeSpeed = 5f;
+    [SerializeField] private float toungeTime = 2f;
+    [SerializeField] private KeyCode keyCodeTounge;
+    [SerializeField] private GameObject toungeFrontObj;
+    private Coroutine toungeCoroutine = null;
+    private Rigidbody2D toungeRigid;
+	// Use this for initialization
+	void Start () {
+        toungeRigid = toungeFrontObj.GetComponent<Rigidbody2D>();
+        if(toungeRigid==null){
+            Debug.LogError("舌オブジェクトにRigidbody2Dがアタッチされていません。");
+        }
+        }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(keyCodeTounge)) {
+            if (toungeCoroutine != null)
+            {
+                StopCoroutine(toungeCoroutine);
+            }
+            toungeCoroutine = StartCoroutine(ToungeCorotine());
+        }
+	}
+
+    private IEnumerator ToungeCorotine(){
+        ResetTounge();
+        var inputVec = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        toungeRigid.velocity = inputVec * toungeSpeed;
+        yield return new WaitForSecondsRealtime(toungeTime);
+        toungeCoroutine = null;
+    }
+
+    private void ResetTounge()
+    {
+        toungeRigid.velocity = Vector2.zero;
+        toungeFrontObj.transform.position = this.transform.position;
+    }
+}
