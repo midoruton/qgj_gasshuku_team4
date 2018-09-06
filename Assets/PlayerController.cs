27368a015b4ce60ec2a09fe2fd70d4f95b116a83
LAPTOP-SSQC0SPI\B_tow <1754hogefooo@gmail.com>
2018-09-06 10:18:24 +0900

    playerControllerの内部を実装 （横移動、地上でのみジャンプ）

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,13 +4,50 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public float moveSpeed;
+    public float jumpSpeed;
+
+    private Rigidbody2D rb2D;
+    private bool ableToJump = false;
+
+    // Use this for initialization
+    void Start() {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Vector2 newVel = new Vector2(0,0);
+        if (Input.GetKey(KeyCode.D))
+        {
+            newVel.x += moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            newVel.x += -moveSpeed;
+        }
+        if (ableToJump&&Input.GetKeyDown(KeyCode.W))
+        {
+            newVel.y = jumpSpeed;
+        }
+        newVel.y += rb2D.velocity.y;
+        rb2D.velocity = newVel;
 	}
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.gameObject.transform.parent.name == "Leaves")
+        {
+            Debug.Log("on leave");
+            ableToJump = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.transform.parent.name == "Leaves")
+        {
+            ableToJump = false;
+        }
+    }
 }
