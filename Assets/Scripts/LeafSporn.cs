8e55e2360e52f64060f08f9531e639c017e2b0ce
LAPTOP-O1DI2PDB\nouberi <nouberium@gmail.com>
2018-09-07 10:41:34 +0900

    スコアの実装、葉っぱの仕様の変更

--- Assets/Scripts/LeafSporn.cs
+++ Assets/Scripts/LeafSporn.cs
@@ -7,12 +7,16 @@ public class LeafSporn : MonoBehaviour {
     public GameObject Leaf;
     public float timespan = 3.0f;
     private float _timer;
+    private float _spornPos;
+    private float _beforePos;
 
 	// Use this for initialization
 	void Start ()
     {
+        _spornPos = Random.Range(-6.0f, 6.0f);
         _timer = 0.0f;
-        Instantiate(Leaf, new Vector2(Random.Range(-6.0f, 6.0f), 15.0f), transform.rotation);
+        Instantiate(Leaf, new Vector2(_spornPos, 15.0f), transform.rotation);
+        _beforePos = _spornPos;
 	}
 	
 	// Update is called once per frame
@@ -20,8 +24,13 @@ public class LeafSporn : MonoBehaviour {
     {
         if(_timer >= timespan)
         {
-            Instantiate(Leaf,new Vector2(Random.Range(-6.0f,6.0f),15.0f),transform.rotation);
+            while(Mathf.Abs(_spornPos - _beforePos) < 2.5f)
+            {
+                _spornPos = Random.Range(-6.0f, 6.0f);
+            }
+            Instantiate(Leaf,new Vector2(_spornPos, 15.0f), transform.rotation);
             _timer = 0.0f;
+            _beforePos = _spornPos;
         }
 
         _timer += Time.deltaTime;
