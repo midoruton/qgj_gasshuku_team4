4fca195233deb6605c0d9fb1280e4f9540fc75c6
midorun <midoruton0906@gmail.com>
2018-09-07 14:28:14 +0900

    カメラ調整

--- Assets/Scripts/PlayerHitpoint.cs
+++ Assets/Scripts/PlayerHitpoint.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerHitpoint : MonoBehaviour {
 
     [SerializeField] GameObject splashEffect;
+    [SerializeField] private AudioClip fallClip;
+    [SerializeField] private Transform SpawnPoint;
+     
     public int DeathCounter = 0;
 
 	// Use this for initialization
@@ -23,11 +26,11 @@ public class PlayerHitpoint : MonoBehaviour {
         if(col.tag == "Water")
         {
             Instantiate(splashEffect, this.transform.position, Quaternion.identity);
-            transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+            transform.position = SpawnPoint.transform.position;
             this.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
             DeathCounter += 1;
-            Debug.Log(DeathCounter);
-
+            this.GetComponent<AudioSource>().clip = fallClip;
+            this.GetComponent<AudioSource>().Play();
         }
     }
 }
