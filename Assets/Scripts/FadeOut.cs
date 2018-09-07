5a557474730f683e98b1a30fb790044b8d440c16
LAPTOP-O1DI2PDB\nouberi <nouberium@gmail.com>
2018-09-07 14:03:31 +0900

    go!とそこまで!の表示

---
+++ Assets/Scripts/FadeOut.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOut : MonoBehaviour {
+
+    private float _timer = 0.0f;
+    private SpriteRenderer _spriteRenderer;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        Destroy(gameObject, 0.5f);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = new Vector3(0.3f + _timer * 5, 0.3f + _timer * 5, 1);
+        _spriteRenderer.color = new Color(255, 255, 255, 2 * (0.5f - _timer));
+        _timer += Time.deltaTime;
+	}
+}
